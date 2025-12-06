9f963a59a74fc38e119ba8eac63aa5f48a4f6773
AyushPoojariUCD <ayush.poojari@ucdconnect.ie>
2025-12-06 17:50:48 +0530

    feat: database setup

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default defineConfig({
+  dialect: "postgresql",
+
+  schema: "./src/models/*.js",
+  out: "./drizzle",
+
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
