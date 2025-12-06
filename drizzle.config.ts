ca2c9d8e4b7fe97d90efd8921a40c2b5fd8dd8b7
AyushPoojariUCD <ayush.poojari@ucdconnect.ie>
2025-12-06 18:07:04 +0530

    feat: github workflows

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "drizzle-kit";
-import dotenv from "dotenv";
+import { defineConfig } from 'drizzle-kit';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 export default defineConfig({
-  dialect: "postgresql",
+  dialect: 'postgresql',
 
-  schema: "./src/models/*.js",
-  out: "./drizzle",
+  schema: './src/models/*.js',
+  out: './drizzle',
 
   dbCredentials: {
     url: process.env.DATABASE_URL!,
